8e229672ef36e77b5a5d84f1e2ea279b587470e4
rajan-garg <iamrajangarg@gmail.com>
2017-04-16 14:56:08 +0530

    neo4j work done suggested videos are shown

--- connect.py
+++ connect.py
@@ -5,11 +5,16 @@ from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
 import json
+from py2neo import Graph, authenticate
+
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'mydb'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/mydb'
+
+authenticate("localhost:7474", "neo4j", "ronalpha")
+graph = Graph();
 
 mongo = PyMongo(app)
 
@@ -17,13 +22,18 @@ mongo = PyMongo(app)
 def show(vidID):
 	star = mongo.db.mycol
 	a = star.find_one({'videoInfo.id':vidID})
-	return render_template('index.html', videoid = a)
+	b=graph.run("match(n:Youtube)-[r:`SAME_CHANNEL`]-(n2) where n.name= {n} return n2 limit 10",n = vidID)
+	out=[]
+	for i in b:
+		out.append(star.find_one({'videoInfo.id':i['n2']['name']}))
+
+	return render_template('index.html',name=out , videoid = a)
 
 @app.route('/success/<query>')
 def success(query):
 	star = mongo.db.mycol
 	a = star.find( { '$text': { '$search': query } }, { 'score': {'$meta': "textScore"}}).limit(10)
-	return render_template('index.html', name = a , q=query)
+	return render_template('index.html', name = a)
 
 @app.route('/index',methods = ['POST', 'GET'])
 def login():
