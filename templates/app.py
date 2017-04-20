897ca39ddb5e227f3fb3fa6f61fb99b0c21ad2b1
rajan-garg <iamrajangarg@gmail.com>
2017-04-20 18:30:09 +0530

    did you mean added

--- templates/app.py
+++ templates/app.py
@@ -7,9 +7,6 @@ from flask_pymongo import PyMongo
 import json
 from py2neo import Graph, authenticate
 
-import re
-from collections import Counter
-
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'mydb'
@@ -30,44 +27,6 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'ronalpha'
 app.config['MYSQL_DATABASE_DB'] = 'youtube'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
-
-
-def words(text): return re.findall(r'\w+', text.lower())
-
-WORDS = Counter(words(open('taglist.txt').read()))
-
-def P(word, N=sum(WORDS.values())): 
-    "Probability of `word`."
-    return WORDS[word] / N
-
-def correction(word): 
-    "Most probable spelling correction for word."
-    return max(candidates(word), key=P)
-
-def candidates(word): 
-    "Generate possible spelling corrections for word."
-    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
-
-def known(words): 
-    "The subset of `words` that appear in the dictionary of WORDS."
-    return set(w for w in words if w in WORDS)
-
-def edits1(word):
-    "All edits that are one edit away from `word`."
-    letters    = 'abcdefghijklmnopqrstuvwxyz'
-    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-    deletes    = [L + R[1:]               for L, R in splits if R]
-    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-    inserts    = [L + c + R               for L, R in splits for c in letters]
-    return set(deletes + transposes + replaces + inserts)
-
-
-
-def edits2(word): 
-    "All edits that are two edits away from `word`."
-    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
-
 
 @app.route('/video/<vidID>')
 def video(vidID):
@@ -161,27 +120,10 @@ def success(query):
 	print query
 	star = mongo.db.mycol
 	a = star.find( { '$text': { '$search': query } }, { 'score': {'$meta': "textScore"}}).sort([('score',{'$meta':'textScore'})]).limit(11)
-	b=[]
-	count = 0
-	for yolo in a:
-		b.append(yolo)
-		count += 1
-	if count >0:
-		if acc=='':
-			return render_template('index.html',name = b , answer= query)
-		else:
-			return render_template('welcome.html',username=acc,name = b , answer= query)
+	if acc=='':
+		return render_template('index.html',name = a)
 	else:
-		input_str = query
-		cor_list = []
-		for input_words in input_str.split():
-			cor_list.append( correction(input_words) )
-		print "output is:"
-		query1=" ".join(cor_list)
-		if query1==query:
-			return redirect(url_for('home'))
-		else:
-			return redirect(url_for('success',query = query1))
+		return render_template('welcome.html',username=acc,name = a)
 
 @app.route('/search',methods = ['POST', 'GET'])
 def search():
